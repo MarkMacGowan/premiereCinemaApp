32c407b22908cba2d6983467478ff9d06133e73e
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-07-25 07:40:52 +0100

    feat: add login and signup pages

---
+++ login.php
@@ -0,0 +1,44 @@
+<?php
+
+         
+        
+        $db = new SQLite3('data.sqlite3');
+
+        
+        $db->exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)");
+
+        
+        $db->exec("INSERT INTO users (username, password) VALUES ('admin', '1234')");
+
+        
+        $result = $db->query("SELECT * FROM users");
+
+       /*  while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+            echo "User: " . $row['username'] . "<br>";
+        } */
+
+
+
+
+    ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Log In</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>     
+    <body class="container mt-5">
+            <h1> Login</h1>
+            <form method="$post" action="$login.php">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" name="username" id="username" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" id="password" class="form-control">
+                    </div>
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+    </body>
+</html>
